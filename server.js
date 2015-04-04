8cc0c3aa3e6ac726357085cf83ed0d4402452af2
Andreas Caspers <casperli@users.noreply.github.com>
2015-04-04 13:06:25 +0200

    Initial commit Part 2

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var app = express();
+var morgan = require('morgan');
+var fs = require('fs');
+
+// create a write stream (in append mode)
+var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'})
+
+// setup the logger
+app.use(morgan('combined', {stream: accessLogStream}))
+
+app.use('/', function (req, res) {
+    res.send('Hello World');
+});
+
+app.listen(3000);
+
+module.exports = app;
+console.log("Node is running at http://localhost:3000");
+
+process.on('SIGINT', function () {
+    console.log('Exiting MogBoC server');
+    process.exit();
+});
