df1adf30f904165cdaa04a53d2e55e62b1aab411
Andreas Caspers <casperli@users.noreply.github.com>
2015-04-05 11:20:07 +0200

    More test DB code added

--- server.js
+++ server.js
@@ -11,25 +11,31 @@ module.exports = app;
 
 console.log("MogBoC server is running at http://localhost:3000");
 
-//var sqlite3 = require('sqlite3').verbose();
-//var db = new sqlite3.Database(':memory:');
-//db.serialize(function(){
-//    db.run("CREATE TABLE lorem (info TEXT)");
-//
-//    var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-//    for(var i = 0; i <10; i++)
-//    {
-//        stmt.run("Ipsum" + i);
-//    }
-//
-//    stmt.finalize();
-//
-//    db.each("SELECT rowid as id, info FROM lorem", function (err,row){
-//        console.log(row.id + ": " + row.info);
-//    });
-//});
-//
-//db.close();
+var sqlite3 = require("sqlite3").verbose();
+var db = new sqlite3.Database("mogboc.db");
+db.serialize(function () {
+    db.run("DROP TABLE IF EXISTS EngineRpm");
+    db.run("CREATE TABLE IF NOT EXISTS EngineRpm (id INTEGER PRIMARY KEY, time INTEGER, rpm INTEGER)");
+
+    db.run("BEGIN TRANSACTION");
+    var stmt = db.prepare("INSERT INTO EngineRpm (time, rpm) VALUES (datetime(), ?)");
+    for (var i = 0; i < 10; i++) {
+        stmt.run(500 + i);
+    }
+
+    stmt.finalize();
+
+    db.run("COMMIT TRANSACTION");
+
+    db.each("SELECT time, avg(rpm) as rpm FROM EngineRpm GROUP BY time", function (err, row) {
+        if (err) {
+            console.log("Error: " + err);
+        }
+        console.log("Time: " + row.time + " Rpm: " + row.rpm);
+    });
+});
+
+db.close();
 
 process.on("SIGINT", function () {
     console.log("Exiting MogBoC server");
