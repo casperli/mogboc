b0918999ac275bad1e201c51b9198d295a5aa8b5
Andreas Caspers <casperli@users.noreply.github.com>
2015-04-06 08:42:46 +0200

    Animation of tachometer fixed.

--- public/cockpit/controllers/cockpit.client.controller.js
+++ public/cockpit/controllers/cockpit.client.controller.js
@@ -1,14 +1,8 @@
 "use strict";
 
-//See: https://github.com/pablojim/highcharts-ng
 var cockpit = angular.module("cockpit", ["highcharts-ng"]);
 
 cockpit.controller("CockpitController", function ($scope) {
-
-    //$scope.toggleLoading = function () {
-    //    this.chartConfig.loading = !this.chartConfig.loading
-    //}
-
     $scope.chartConfig = {
         options: {
             chart: {
@@ -25,70 +19,80 @@ cockpit.controller("CockpitController", function ($scope) {
                     backgroundColor: {
                         linearGradient: {x1: 0, y1: 0, x2: 0, y2: 1},
                         stops: [
-                            [0, '#FFF'],
-                            [1, '#333']
+                            [0, "#FFF"],
+                            [1, "#333"]
                         ]
                     },
                     borderWidth: 0,
-                    outerRadius: '109%'
+                    outerRadius: "109%"
                 }, {
                     backgroundColor: {
                         linearGradient: {x1: 0, y1: 0, x2: 0, y2: 1},
                         stops: [
-                            [0, '#333'],
-                            [1, '#FFF']
+                            [0, "#333"],
+                            [1, "#FFF"]
                         ]
                     },
                     borderWidth: 1,
-                    outerRadius: '107%'
+                    outerRadius: "107%"
                 }, {
                     // default background
                 }, {
-                    backgroundColor: '#DDD',
+                    backgroundColor: "#DDD",
                     borderWidth: 0,
-                    outerRadius: '105%',
-                    innerRadius: '103%'
+                    outerRadius: "105%",
+                    innerRadius: "103%"
                 }]
             },
             yAxis: {
                 min: 0,
                 max: 2800,
 
-                minorTickInterval: 'auto',
+                minorTickInterval: "auto",
                 minorTickWidth: 1,
                 minorTickLength: 10,
-                minorTickPosition: 'inside',
-                minorTickColor: '#666',
+                minorTickPosition: "inside",
+                minorTickColor: "#666",
 
                 tickPixelInterval: 30,
                 tickWidth: 2,
-                tickPosition: 'inside',
+                tickPosition: "inside",
                 tickLength: 10,
-                tickColor: '#666',
+                tickColor: "#666",
                 labels: {
                     step: 2,
-                    rotation: 'auto'
+                    rotation: "auto"
                 },
                 title: {
-                    text: 'rpm'
+                    text: "rpm"
                 },
                 plotBands: [{
                     from: 0,
                     to: 500,
-                    color: '#DDDF0D' // yellow
+                    color: "#DDDF0D" // yellow
                 }, {
                     from: 500,
                     to: 2300,
-                    color: '#55BF3B' // green
+                    color: "#55BF3B" // green
                 }, {
                     from: 2300,
                     to: 2550,
-                    color: '#DDDF0D' // yellow
+                    color: "#DDDF0D" // yellow
                 }, {
                     from: 2550,
                     to: 2800,
-                    color: '#DF5353' // red
+                    color: "#DF5353" // red
                 }]
+            },
+            plotOptions: {
+                gauge: {
+                    dial: {
+                        baseWidth: 5,
+                        topWidth: 1,
+                        baseLength: "80%", // of radius
+                        rearLength: "10%"
+                    }
+                }
             }
         },
 
@@ -103,24 +107,10 @@ cockpit.controller("CockpitController", function ($scope) {
                 valueSuffix: " rpm"
             }
         }]
-    }
+    };
 
     $scope.update = function () {
         var value = parseInt($scope.rpm);
-        //$scope.chartConfig.series[0].setData(value, true);
-
-        $scope.chartConfig.getHighcharts().series[0].points[0].update(value);
-        //var start = $scope.chartConfig.series[0].data[0];
-        //
-        //for (var i = start; i < value; i++) {
-        //    window.setTimeout(function () {
-        //        $scope.chartConfig.series[0].data[0] = value;
-        //    }, 10);
-        //
-        //}
-
-
-        //var point = $scope.chartConfig.series[0].points[0];
-        //point.update(parseInt($scope.rpm));
+        $scope.chartConfig.getHighcharts().series[0].points[0].update(value, true, {"duration": 500});
     };
 });
